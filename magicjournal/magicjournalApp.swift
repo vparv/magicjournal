00276958c4b2bf173728abc675756cddf5d496f5
Vineet Parvathala <vparv@umich.edu>
2024-12-11 19:40:01 -0800

    Initial Commit

---
+++ magicjournal/magicjournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  magicjournalApp.swift
+//  magicjournal
+//
+//  Created by Vineet Parvathala on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct magicjournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
